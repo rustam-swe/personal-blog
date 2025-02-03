d7a20258aa21e58361f9972e77a863b9675b53c1
Rustam <rustamergashev.sp@gmail.com>
2025-02-03 19:44:57 +0500

    Initial commit: front and back and db connected. Create post implemented

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<h1>Personal Blog</h1>
+<form action="index.php" method="post">
+  <input type="text" name="title" placeholder="Title"><br>
+  <textarea name="text"></textarea><br>
+<button type="submit">Submit</button>
+</form>
+</html>
+
+<?php
+
+$title = $_POST['title'] ?? null;
+$text = $_POST['text'] ?? null;
+$date = date('Y-m-d H:i:s');
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=personal_blog", 'root', '1234');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$stmt = $db->prepare("INSERT INTO posts(title, text, created_at) VALUES(:title, :text, :created_at)");
+$stmt->execute(['title'=>$title, 'text'=>$text, 'created_at' => $date]);
+
+
+
